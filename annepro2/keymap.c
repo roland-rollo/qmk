e8c794ea4c24c2c1fe6d487411a957f28a04a52b
roland-rollo <syntezoid@protonmail.com>
2022-04-03 09:36:00 +0200

    fuck arrow keys on right side

--- annepro2/keymap.c
+++ annepro2/keymap.c
@@ -47,9 +47,9 @@ enum anne_pro_layers {
 * |-----------------------------------------------------------------------------------------+
 * |  SCRL   |     |     |     |     |     |     |     |     |     |     |     |             |
 * |-----------------------------------------------------------------------------------------+
-* |            |     |     |     |     |     |     |     |     |     |     |       UP       |
+* |            |     |     |     |     |     |     |     |     |     |     |                |
 * |-----------------------------------------------------------------------------------------+
-* |       |       |       |                                 |       |  LEFT | DOWN  | RIGHT |
+* |       |       |       |                                 |       |       |       |       |
 * \-----------------------------------------------------------------------------------------/
 */
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -57,8 +57,8 @@ enum anne_pro_layers {
     KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
     KC_TAB,           KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
     LT(FN4, KC_SCRL), KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN,          KC_QUOT,          KC_ENT,
-    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
-    KC_LCTL,          KC_LGUI, KC_LALT,                   KC_SPC,             KC_RALT, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
+    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          KC_RSFT,
+    KC_LCTL,       KC_LGUI, KC_LALT,                    KC_SPC,               KC_RALT, MO(FN1),          MO(FN2),          KC_RCTL
 ),
   /*
   * Layer FN1
