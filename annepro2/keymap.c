83fad8f001d5ad8c39b13092286b3c5cbeb2c24f
roland-rollo <syntezoid@protonmail.com>
2022-05-13 23:36:53 +0200

    back to Z as first letter in third row

--- annepro2/keymap.c
+++ annepro2/keymap.c
@@ -58,7 +58,7 @@ enum anne_pro_layers {
     KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_EQL, KC_7, KC_8,    KC_9,             KC_0,          KC_MINS,        KC_BSPC,
     KC_TAB,           KC_Q,    KC_W,    KC_F, KC_P, KC_B, KC_LBRC,   KC_J, KC_L, KC_U,    KC_Y,             KC_SCLN,          KC_QUOT,       KC_BSLS,
     LT(FN4, KC_SCRL), KC_A,    KC_R,    KC_S, KC_T, KC_G, KC_RBRC,   KC_M, KC_N, KC_E,    KC_I,          KC_O,          KC_ENT,
-    KC_LSFT,                   KC_X,    KC_C, KC_D, KC_V, KC_Z,   KC_SLSH, KC_K, KC_H, KC_COMM,           KC_DOT,          KC_RSFT,
+    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_D, KC_V,   KC_SLSH, KC_K, KC_H, KC_COMM,           KC_DOT,          KC_RSFT,
     KC_LCTL,       KC_LGUI, KC_LALT,                    KC_SPC,               KC_RALT, MO(FN1),          MO(FN2),          KC_RCTL
 ),
   /*
