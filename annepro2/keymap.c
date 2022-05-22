c20486db304a4fc063d53ade3d51743b858b942f
roland-rollo <syntezoid@protonmail.com>
2022-05-22 14:44:30 +0200

    i'm defeated

--- annepro2/keymap.c
+++ annepro2/keymap.c
@@ -55,10 +55,10 @@ enum anne_pro_layers {
 */
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [BASE] = LAYOUT_60_ansi( /* Base */
-    KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_EQL, KC_7, KC_8,    KC_9,             KC_0,          KC_MINS,        KC_BSPC,
-    KC_TAB,           KC_Q,    KC_W,    KC_F, KC_P, KC_B, KC_LBRC,   KC_J, KC_L, KC_U,    KC_Y,             KC_SCLN,          KC_QUOT,       KC_BSLS,
-    LT(FN4, KC_SCRL), KC_A,    KC_R,    KC_S, KC_T, KC_G, KC_RBRC,   KC_M, KC_N, KC_E,    KC_I,          KC_O,          KC_ENT,
-    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_D, KC_V,   KC_SLSH, KC_K, KC_H, KC_COMM,           KC_DOT,          KC_RSFT,
+    KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
+    KC_TAB,           KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
+    LT(FN4, KC_SCRL), KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN,          KC_QUOT,          KC_ENT,
+    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          KC_RSFT,
     KC_LCTL,       KC_LGUI, KC_LALT,                    KC_SPC,               KC_RALT, MO(FN1),          MO(FN2),          KC_RCTL
 ),
   /*
@@ -162,7 +162,7 @@ enum anne_pro_layers {
   * |-----------------------------------------------------------------------------------------+
   * | LCtrl |  L1   | LAlt  |               space             | RAlt  |  FN1  |  FN2  | RCtrl |
   * \-----------------------------------------------------------------------------------------/
-  * Layer TAP in BASE
+  * Layer TAP in FN5
   * ,-----------------------------------------------------------------------------------------.
   * |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
   * |-----------------------------------------------------------------------------------------+
@@ -176,10 +176,10 @@ enum anne_pro_layers {
   * \-----------------------------------------------------------------------------------------/
   */
  [FN5] = LAYOUT_60_ansi( /* FN5 */
-    KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
-    KC_TAB,           KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
-    LT(FN4, KC_SCRL), KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN,          KC_QUOT,          KC_ENT,
-    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          KC_RSFT,
+    KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_EQL, KC_7, KC_8,    KC_9,             KC_0,          KC_MINS,        KC_BSPC,
+    KC_TAB,           KC_Q,    KC_W,    KC_F, KC_P, KC_B, KC_LBRC,   KC_J, KC_L, KC_U,    KC_Y,             KC_SCLN,          KC_QUOT,       KC_BSLS,
+    LT(FN4, KC_SCRL), KC_A,    KC_R,    KC_S, KC_T, KC_G, KC_RBRC,   KC_M, KC_N, KC_E,    KC_I,          KC_O,          KC_ENT,
+    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_D, KC_V,   KC_SLSH, KC_K, KC_H, KC_COMM,           KC_DOT,          KC_RSFT,
     KC_LCTL,       KC_LGUI, KC_LALT,                    KC_SPC,               KC_RALT, MO(FN1),          TG(FN5),          KC_RCTL
  ),
 };
